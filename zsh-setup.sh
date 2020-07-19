3cbaec4584a9cbb77548ce4a89be55d4a73f8315
Thomas Woltjer <thomas@thomaswoltjer.com>
2020-07-19 11:18:06 -0400

    Add some lines for troubleshooting

--- zsh-setup.sh
+++ zsh-setup.sh
@@ -3,6 +3,8 @@ set -e
 echo "Cloning submodules"
 git submodule update --init --recursive
 echo "Copying \"Oh My Zsh\" to home directory"
+pwd
+ls
 cp -r ohmyszh ~/.oh-my-zsh
 echo "Copying zsh configuration file to home directory"
 cp -v zshrc ~/.zshrc
