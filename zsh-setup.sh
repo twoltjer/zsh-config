f9e25afc7450b4bf8869ef44b093f6eb1fd73542
Thomas Woltjer <thomas@thomaswoltjer.com>
2020-07-19 11:21:58 -0400

    Fix typo

--- zsh-setup.sh
+++ zsh-setup.sh
@@ -3,9 +3,7 @@ set -e
 echo "Cloning submodules"
 git submodule update --init --recursive
 echo "Copying \"Oh My Zsh\" to home directory"
-pwd
-ls
-cp -r ohmyszh ~/.oh-my-zsh
+cp -r ohmyzsh ~/.oh-my-zsh
 echo "Copying zsh configuration file to home directory"
 cp -v zshrc ~/.zshrc
 echo "Done"
