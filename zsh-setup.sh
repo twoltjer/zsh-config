a050ca24d785aefc1ba293da180c641a5ea7d74b
Thomas Woltjer <thomas@thomaswoltjer.com>
2020-07-19 11:12:31 -0400

    Try a different home path

--- zsh-setup.sh
+++ zsh-setup.sh
@@ -3,7 +3,7 @@ set -e
 echo "Cloning submodules"
 git submodule update --init --recursive
 echo "Copying \"Oh My Zsh\" to home directory"
-cp -r ohmyszh $HOME/.oh-my-zsh
+cp -r ohmyszh ~/.oh-my-zsh
 echo "Copying zsh configuration file to home directory"
-cp -v zshrc $HOME/.zshrc
+cp -v zshrc ~/.zshrc
 echo "Done"
